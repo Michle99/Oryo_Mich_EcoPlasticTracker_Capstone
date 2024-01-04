7f43feb422a9bd51824478f7242162b6ca5baf09
Michle99 <sammiyonyx@gmail.com>
2024-01-04 12:31:06 -0500

    updated `db/index` with refactored db conn

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,2 +1,16 @@
 import mongoose from 'mongoose';
 
+const mongodb = process.env.MONGO_URI || "";
+
+
+const connectDB = async (): Promise<void> => {
+    try {
+      await mongoose.connect(mongodb);
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      process.exit(1);
+    }
+  };
+  
+  export default connectDB;
