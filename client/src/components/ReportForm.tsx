201cc514418534cd09d6450316b314010bcd17ac
Michle99 <sammiyonyx@gmail.com>
2024-01-09 10:29:58 -0500

    updated `ReportForm` with state variables

--- client/src/components/ReportForm.tsx
+++ client/src/components/ReportForm.tsx
@@ -1,2 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import LocationPicker from "react-location-picker";
+
+
+interface ReportFormProps {
+    onSubmitSuccess: () => void;
+}
+
+const ReportForm: React.FC<ReportFormProps> = ({ onSubmitSuccess }) => {
+    const [location, setLocation] = useState({ lat: 0, lng: 0 });
+    const [type, setType] = useState<string>('');
+    const [images, setImages] = useState<string[]>([]);
+    const [imageInput, setImageInput] = useState<string>('');
+
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default ReportForm;
