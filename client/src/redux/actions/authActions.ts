d3ab81515b04839cdc68aeb4246087972446d7be
Michle99 <sammiyonyx@gmail.com>
2024-01-12 01:28:39 -0500

    updated `authActions.ts`

---
+++ client/src/redux/actions/authActions.ts
@@ -0,0 +1,26 @@
+// src/redux/actions/authActions.ts
+import { Dispatch } from 'redux';
+import axios from 'axios';
+import { AuthActionTypes, AuthAction } from './types';
+
+export const signup = (userData: { email: string; username: string; password: string }) => {
+  return async (dispatch: Dispatch<AuthActionTypes>) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/signup', userData);
+      dispatch({ type: 'SIGNUP_SUCCESS', payload: response.data });
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+};
+
+export const login = (userData: { email: string; password: string }) => {
+  return async (dispatch: Dispatch<AuthActionTypes>) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/login', userData);
+      dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+};
