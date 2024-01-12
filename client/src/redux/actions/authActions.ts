192a0bc23b95d9f965deec6d0833331a17d7eb99
Michle99 <sammiyonyx@gmail.com>
2024-01-12 09:36:01 -0500

    updated `authActions.ts`

--- client/src/redux/actions/authActions.ts
+++ client/src/redux/actions/authActions.ts
@@ -1,26 +1,28 @@
 // src/redux/actions/authActions.ts
 import { Dispatch } from 'redux';
 import axios from 'axios';
-import { AuthActionTypes } from './types';
+import { AuthActionTypes, SIGNUP_SUCCESS, LOGIN_SUCCESS } from './types';
 
-export const signup = (userData: { email: string; username: string; password: string }) => {
-  return async (dispatch: Dispatch<AuthActionTypes>) => {
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/signup', userData);
-      dispatch({ type: 'SIGNUP_SUCCESS', payload: response.data });
-    } catch (error) {
-      console.error('Error signing up:', error);
-    }
-  };
+interface UserData {
+  email: string;
+  username?: string;
+  password: string;
+}
+
+export const signup = (userData: UserData) => async (dispatch: Dispatch<AuthActionTypes>) => {
+  try {
+    const response = await axios.post('http://localhost:3000/api/auth/signup', userData);
+    dispatch({ type: SIGNUP_SUCCESS, payload: response.data });
+  } catch (error) {
+    console.error('Error signing up:', error);
+  }
 };
 
-export const login = (userData: { email: string; password: string }) => {
-  return async (dispatch: Dispatch<AuthActionTypes>) => {
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', userData);
-      dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
-    } catch (error) {
-      console.error('Error logging in:', error);
-    }
-  };
+export const login = (userData: UserData) => async (dispatch: Dispatch<AuthActionTypes>) => {
+  try {
+    const response = await axios.post('http://localhost:3000/api/auth/login', userData);
+    dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+  } catch (error) {
+    console.error('Error logging in:', error);
+  }
 };
