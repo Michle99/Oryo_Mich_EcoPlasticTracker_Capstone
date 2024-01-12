53cb4ea9e12eb1a77f0665cdab71c712d6cfd230
Michle99 <sammiyonyx@gmail.com>
2024-01-12 01:27:20 -0500

    installed `redux` deps, add `SignUp` comp

---
+++ client/src/components/SignUp.tsx
@@ -0,0 +1,69 @@
+// src/components/Signup.tsx
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import { useDispatch } from 'react-redux';
+// import { signup } from '../redux/actions/authActions';
+
+const Signup: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    dispatch(signup({ email, username, password }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Typography variant="h5" gutterBottom>
+        Sign Up
+      </Typography>
+      <TextField
+        label="Email"
+        variant="outlined"
+        fullWidth
+        value={email}
+        onChange={handleEmailChange}
+        required
+      />
+      <TextField
+        label="Username"
+        variant="outlined"
+        fullWidth
+        value={username}
+        onChange={handleUsernameChange}
+        required
+      />
+      <TextField
+        label="Password"
+        type="password"
+        variant="outlined"
+        fullWidth
+        value={password}
+        onChange={handlePasswordChange}
+        required
+      />
+      <Button type="submit" variant="contained" color="primary" sx={{ marginTop: 2 }}>
+        Sign Up
+      </Button>
+    </form>
+  );
+};
+
+export default Signup;
