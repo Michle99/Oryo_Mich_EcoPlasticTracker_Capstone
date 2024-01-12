6165bf420dfa3f9180129d8b914be385c3c46f78
Michle99 <sammiyonyx@gmail.com>
2024-01-12 00:31:05 -0500

    added `Home` comp

---
+++ client/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+
+
+const Home = () => {
+    return (
+        <Container>
+            <Typography variant="h1">
+                Welcome to EcoPlastic Tracker Web Application.
+            </Typography>
+        </Container>
+    );
+}
+
+export default Home;
