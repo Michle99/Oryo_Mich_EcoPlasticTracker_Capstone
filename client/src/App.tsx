fd771eea1e56b492f6819875a7a168583749c46f
Michle99 <sammiyonyx@gmail.com>
2024-01-14 17:38:33 -0500

    updated `App.tsx` & added `Login.tsx`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,33 @@
-// import './App.css'
-import Home from './components/Home';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { CssBaseline, Container, Paper } from '@mui/material';
+// import Header from './components/Header';
+import Home from './components/Home';
+import Signup from './components/SignUp';
+import Login from './components/Login';
 
 const App: React.FC = () => {
-  // const handleReportSubmission = () => {
-  //   console.log("Report submitted successfully");
-  // }  
-
   return (
     <Router>
-        <Routes>
-         <Route path='/' element={<Home/>}/>
-        </Routes>
+      <CssBaseline />
+      <Container component="main" maxWidth="lg">
+        {/* <Header /> */}
+        <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+          <Routes>
+            <Route path="/" >
+              <Home />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Routes>
+        </Paper>
+      </Container>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
