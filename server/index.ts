6f9304f3fd75a65e6ed2482fca610d66c9797299
Michle99 <sammiyonyx@gmail.com>
2024-01-15 15:37:06 -0500

    updated `cors` config

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,10 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors({
+  origin: " http://localhost:5173",
+  credentials: true,
+}));
 
 // MongoDB Connection
 connectDB();
