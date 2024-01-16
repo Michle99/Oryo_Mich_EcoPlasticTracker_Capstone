520626613ac1486093fbcf74bb4a226f73393e80
Michle99 <sammiyonyx@gmail.com>
2024-01-16 17:59:47 -0500

    added `submitReport` func in `reportSlice.ts`

--- client/src/redux/reportSlice.ts
+++ client/src/redux/reportSlice.ts
@@ -6,3 +6,8 @@ export const fetchReports = createAsyncThunk('reports/fetchReports', async () =>
     const response = await axios.get('http://localhost:3000/api/reports/all');
     return response.data;
 });
+
+export const submitReport = createAsyncThunk('reports/submitReport', async (reportData: any) => {
+    const response = await axios.post('http://localhost:3000/api/reports/submit', reportData);
+    return response.data;
+});
