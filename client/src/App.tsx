315cf3aa52082e92c82507b190fa5638d7332b93
Michle99 <sammiyonyx@gmail.com>
2024-01-22 18:05:21 -0500

    updated `Header` comp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { CssBaseline, Container, Paper } from '@mui/material';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Home from './components/Home';
 import Signup from './components/SignUp';
 import Login from './components/Login';
@@ -11,7 +11,7 @@ import ReportList from './components/ReportList';
 const App: React.FC = () => {
   return (
     <Router>
-      {/* <Header /> */}
+      <Header />
       <CssBaseline />
       <Container 
         component="main"
