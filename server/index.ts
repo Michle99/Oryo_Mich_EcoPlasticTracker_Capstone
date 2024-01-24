fd95474b4befd442212b5773b18de9e2d21a7d74
Michle99 <sammiyonyx@gmail.com>
2024-01-24 15:22:02 -0500

    added `CORS` error resolutions to `server/index`

--- server/index.ts
+++ server/index.ts
@@ -11,11 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 
-app.use((req, res, next) => {
-  res.header('Acess-Control-Allow-Origin', '*');
-  next();
-});
-
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
@@ -23,6 +18,14 @@ app.use(cors({
   optionsSuccessStatus: 200,
   allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 // MongoDB Connection
 connectDB();
