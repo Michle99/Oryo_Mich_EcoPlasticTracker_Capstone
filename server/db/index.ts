23771ccd16851499b05adc71870bf2c9228949a6
Michle99 <sammiyonyx@gmail.com>
2024-01-24 20:53:13 -0500

    updated `mongodb` connection with `dbName`

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 import '../load_envs'
 
-const mongodb = process.env.MONGO_URI || "";
-
-console.log("Connection string:", mongodb);
-
+const mongodbConn = process.env.MONGO_URI || "";
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(mongodb);
+      await mongoose.connect(
+        mongodbConn,
+        {
+          dbName: "environmental_monitoring"
+        }
+      );
       console.log('Connected to MongoDB');
     } catch (error) {
       console.error('Error connecting to MongoDB:', error);
