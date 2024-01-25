71809415666d92ccd78d442ab3b6f984a62979c6
Michle99 <sammiyonyx@gmail.com>
2024-01-24 23:11:55 -0500

    updated with `LoginAlertModal`

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,11 @@
-// components/Header.tsx
-import React from 'react';
-import { 
-  AppBar, 
-  Toolbar, 
-  Typography, 
-  Button, 
-  IconButton, 
-  Menu, 
-  MenuItem 
-} from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout, selectUser } from '../redux/authSlice';
 import { AppDispatch, useAppSelector } from '../redux/store';
 import MenuIcon from '@mui/icons-material/Menu';
+import LoginAlertModal from './modals/LoginAlertModal';
 
 const Header: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -29,7 +21,8 @@ const Header: React.FC = () => {
     }
   };
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [alertOpen, setAlertOpen] = useState(false); // State for the AlertModal
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -39,67 +32,96 @@ const Header: React.FC = () => {
     setAnchorEl(null);
   };
 
+  const handleFormClick = () => {
+    if (user) {
+      // User is authenticated, navigate to the Form page
+      navigate("/submit");
+    } else {
+      // User is not authenticated, open the AlertModal
+      setAlertOpen(true);
+    }
+    handleClose();
+  };
+
+  const handleReportsClick = () => {
+    if (user) {
+      // User is authenticated, navigate to the Reports page
+      navigate("/list");
+    } else {
+      // User is not authenticated, open the AlertModal
+      setAlertOpen(true);
+    }
+    handleClose();
+  };
+
+  const handleAlertClose = () => {
+    setAlertOpen(false);
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleClick}
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem component={Link} to="/" onClick={handleClose}>
-            Home
-          </MenuItem>
-          <MenuItem component={Link} to="/submit" onClick={handleClose}>
-            Form
-          </MenuItem>
-          <MenuItem component={Link} to="/list" onClick={handleClose}>
-            Reports
-          </MenuItem>
-          {user ? (
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          ) : (
-            <MenuItem component={Link} to="/login" onClick={handleClose}>
-              Login
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem component={Link} to="/" onClick={handleClose}>
+              Home
             </MenuItem>
+            <MenuItem onClick={handleFormClick}>
+              Form
+            </MenuItem>
+            <MenuItem onClick={handleReportsClick}>
+              Reports
+            </MenuItem>
+            {user ? (
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            ) : (
+              <MenuItem component={Link} to="/login" onClick={handleClose}>
+                Login
+              </MenuItem>
+            )}
+          </Menu>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            EcoPlastic App
+          </Typography>
+          {user ? (
+            // User is authenticated, show logout button
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          ) : (
+            // User is not authenticated, show login button
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
           )}
-        </Menu>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          EcoPlastic App
-        </Typography>
-        {user ? (
-          // User is authenticated, show logout button
-          <Button color="inherit" onClick={handleLogout}>
-            Logout
-          </Button>
-        ) : (
-          // User is not authenticated, show login button
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <LoginAlertModal isOpen={alertOpen} onClose={handleAlertClose} /> {/* Render the AlertModal */}
+    </>
   );
 };
 
