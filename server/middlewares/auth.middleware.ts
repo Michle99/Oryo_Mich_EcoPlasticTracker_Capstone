69d5ed1427cd267efb2830c5e12f8fc6b24658f7
Michle99 <sammiyonyx@gmail.com>
2024-01-26 14:28:25 -0500

    updates

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -1,21 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import "../load_envs";
 
-const JWT_SECRET = process.env.JWT_SECRET_TOKEN || 'default_secret_key';
+const JWT_SECRET = process.env.JWT_SECRET_TOKEN || "default_secret_key";
 
 // Extend the Request type to include the custom userId property
 interface AuthenticatedRequest extends Request {
   userId?: string;
 }
 
-export const authenticateUser = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+export const authenticateUser = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
 
   console.log("Token from middleware:", token);
 
   if (!token) {
-    res.status(401).json({ error: 'Unauthorized: Missing token' });
+    res.status(401).json({ error: "Unauthorized: Missing token" });
     return;
   }
 
@@ -24,7 +28,7 @@ export const authenticateUser = (req: AuthenticatedRequest, res: Response, next:
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    console.error('Error authenticating user:', error);
-    res.status(401).json({ error: 'Unauthorized: Invalid token' });
+    console.error("Error authenticating user:", error);
+    res.status(401).json({ error: "Unauthorized: Invalid token" });
   }
 };
