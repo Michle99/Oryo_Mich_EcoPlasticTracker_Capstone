602eb538a4c8556c7ed17f52c234c0e8cc31df66
Michle99 <sammiyonyx@gmail.com>
2024-01-28 17:55:44 -0500

    updated `GoogleMapContainer` with `coordinates`

--- client/src/components/PollutionMap.tsx
+++ client/src/components/PollutionMap.tsx
@@ -3,37 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchMapCoordinates } from "../redux/reportSlice";
 import { AppDispatch, RootState } from "../redux/store";
 import { Box } from "@mui/material";
-// import GoogleMapContainer from "./GoogleMap";
+import GoogleMapContainer from "./GoogleMap";
 
 
 const PollutionMap: React.FC = () => {
-    const dispatch: AppDispatch = useDispatch();
-    const [coordinates, setCoordinates] = useState<[number, number][]>([]);
-    const reports = useSelector((state: RootState) => state.report.reports);
+  const dispatch: AppDispatch = useDispatch();
+  const [coordinates, setCoordinates] = useState<[number, number][]>([]);
+  const reports = useSelector((state: RootState) => state.report.reports);
 
-    useEffect(() => {
-      // Fetch pollution reports when the component mounts
-      dispatch(fetchMapCoordinates())
-      .then((response) => {
-        setCoordinates(response.payload || []);
-      });
-    }, [dispatch]);
+  useEffect(() => {
+    // Fetch pollution reports when the component mounts
+    dispatch(fetchMapCoordinates())
+    .then((response) => {
+      setCoordinates(response.payload || []);
+    });
+  }, [dispatch]);
     
-    console.log("Display reports from state:", reports[0]);
-    return (
-        <Box>
-            {/* {reports.map((report) => (
-                <div key={report._id}>
-                    {report.location.coordinates && (
-                      <GoogleMapContainer 
-                        coordinates={report.location.coordinates} 
-                      />
-                    )}
-                </div>
-            ))} */}
+  console.log("Display reports from state:", reports[0]);
+  
+  return (
+    <Box>
+      {coordinates.map((coordinate, index) => (
+        <Box key={index}>
+          <GoogleMapContainer  coordinates={coordinate}/>
         </Box>
-    )
-
+      ))}
+    </Box>
+  )
 };
 
 export default PollutionMap;
